146f7ff4daf205d880b2b1b98de893fc457d6813
Zhu Qiutong <52369223+Zhu-Qiutong@users.noreply.github.com>
2021-11-12 19:29:16 +0800

    Add files via upload

--- Client test.py
+++ Client test.py
@@ -1,39 +1,30 @@
 import socket
 from time import sleep
+import struct
 
 # configure socket and connect to server
 clientSocket = socket.socket()
-host = socket.gethostname()
+host = '127.0.0.2'
 port = 25000
 clientSocket.connect((host, port))
 
-# keep track of connection status
-connected = True
-print("connected to server")
+while True:
+    # sending massage
+    user_input = input("\nPLease enter the data in turn (format: 'opcode data1 data2 ...') : ")
+    opcode = int(user_input.split()[0])
+    data = " ".join(user_input.split()[1:])
+    info_send = struct.pack("I%ds" % (len(user_input.split()) - 1), opcode, bytes(data, "UTF-8"))
+    clientSocket.sendall(info_send)
+
+    # recive massage
+    data = clientSocket.recv(1024)
+    output = bytes.decode(data)
+    print('server back: ', output)
 
 
 
 
 
 
-# while True:
-#     # attempt to send and receive wave, otherwise reconnect
-#     try:
-#         message = clientSocket.recv(1024).decode("UTF-8")
-#         clientSocket.send(bytes("Client wave", "UTF-8"))
-#         print(message)
-#     except socket.error:
-#         # set connection status and recreate socket
-#         connected = False
-#         clientSocket = socket.socket()
-#         print("connection lost... reconnecting")
-#         while not connected:
-#             # attempt to reconnect, otherwise sleep for 2 seconds
-#             try:
-#                 clientSocket.connect((host, port))
-#                 connected = True
-#                 print("re-connection successful")
-#             except socket.error:
-#                 sleep(2)
-#
-# clientSocket.close();
+
+
--- Sever test.py
+++ Sever test.py
@@ -1,26 +1,54 @@
 import socket
 from time import sleep
+import struct
+
 
 # create and configure socket on local host
 serverSocket = socket.socket()
-host = socket.gethostname()
+host = '127.0.0.2'
 port = 25000  # arbitrary port
 serverSocket.bind((host, port))
 serverSocket.listen(1)
 
 con, addr = serverSocket.accept()
-
-print("connected to client")
+print('conected by', addr)
 
 while True:
-    # send wave to client
-    con.send(bytes("Server wave", "UTF-8"))
+    message = con.recv(1024)
+    int_size = struct.calcsize("I")
+    # opcheck, output1 = struct.unpack("I", info_send[:int_size]), info_send[int_size:]
+    opcheck = struct.unpack("I", message[:int_size])
+    opcheck = int(opcheck[0])
 
-    # receive wave from client
-    message = con.recv(1024).decode("UTF-8")
-    print(message)
+    #
+    if opcheck == 1:
+        data_size = 6
+        # con.sendall(str.encode('1'))
 
-    # wait 1 second
-    sleep(1)
+        message = con.recv(1024)
 
-con.close();
+
+
+
+
+
+
+    elif opcheck == 2:
+        data_size = 1
+        con.sendall(str.encode('2'))
+    print(data_size)
+    # output1 = bytes.decode(output1)
+    # output1 = output1.split()
+    # opcheck = int(output1[0])
+
+    # if opcheck == 1:
+    #     sleep(0.5)
+    #     con.sendall(str.encode('1, 0'))
+    #
+    # if opcheck == 3:
+    #     if 0 <= int(output1[1]) <= 100 and 0 <= int(output1[2]) <= 100:
+    #         con.sendall(str.encode("\\3\\0"))
+    #     else:
+    #         con.sendall(str.encode("\\3\\1"))
+
+
