c7b1a54e99b34f8cd29c924d6a9376bd7864ffc6
Zhu Qiutong <52369223+Zhu-Qiutong@users.noreply.github.com>
2021-11-15 11:22:40 +0800

    Add files via upload

--- Client test.py
+++ Client test.py
@@ -16,20 +16,12 @@ port = 25000
 clientSocket.connect((host, port))
 
 while True:
-    # sending massage
-    # user_input = input("\nPLease enter the data in turn (format: 'opcode data1 data2 ...') : ")
-    # opcode = int(user_input.split()[0])
-    # data = " ".join(user_input.split()[1:])
-    #
-    #
-    # info_send = struct.pack("%ds" % (len(user_input.split())), opcode, bytes(data, "UTF-8"))
     user_input = input("\nPLease enter the data in turn (format: 'opcode data1 data2 ...') : ")
     data = user_input.split()
     data = list(map(float, data))
     data = [padding(c) for c in data]
     data = " ".join(data)
     info_send = struct.pack("%ds" % len(data), bytes(data, "UTF-8"))
-    # print(len(data))
     clientSocket.sendall(info_send)
 
 
@@ -38,6 +30,7 @@ while True:
     try:
         check = int(data_recive)
         while check == 1:
+            # print(check)
             clientSocket.sendall(info_send)
             data_recive = clientSocket.recv(1024).decode("UTF-8")
             check = int(data_recive)
@@ -47,11 +40,4 @@ while True:
 
     except ValueError:
         print('Server back: ', data_recive)
-
-
-
-
-
-
-
 
--- Sever test.py
+++ Sever test.py
@@ -5,18 +5,6 @@ import struct
 import random
 
 
-def accident(rate):# rate from: 1 -100
-    a = random.randint(0, 100)
-    counter = 0
-
-    while a > rate:
-        print("retry: ", counter)
-        counter += 1
-        a = random.randint(0, 100)
-
-    print("pass")
-
-
 # create and configure socket on local host
 serverSocket = socket.socket()
 host = '127.0.0.1'
@@ -33,13 +21,6 @@ while True:
     # opcheck, output1 = struct.unpack("I", info_send[:int_size]), info_send[int_size:]
     opcheck = struct.unpack("6s", message[:int_size])
     opcheck = int(float(opcheck[0]))
-    # print(opcheck)
-
-    # data_size = "14s"
-    # data = struct.unpack(data_size, message[int_size:])
-    # double_check = sys.getsizeof(data)
-    # print(double_check)
-
 
 
     if opcheck == 1:
@@ -47,66 +28,50 @@ while True:
         data = struct.unpack(data_size, message[int_size:])
         double_check = sys.getsizeof(data)
 
-        br = random.randint(0,100) # noise
+        br = random.randint(0, 100)  # accident
         if br < 50:
             double_check = 1
+            counter = 1
+            while double_check != 48:
+                con.send(bytes('1', "UTF-8"))
+                message = con.recv(1024)
+                data = struct.unpack(data_size, message[int_size:])
+                double_check = sys.getsizeof(data)
+                br = random.randint(0, 100)  # noise
+                if br < 50:
+                    double_check = 1
+                print(counter)
+                counter += 1
 
-        while double_check != 48:
-            con.send(bytes('1', "UTF-8"))
-
-            message = con.recv(1024)
-            data = struct.unpack(data_size, message[int_size:])
-            double_check = sys.getsizeof(data)
-            accident(10)
-
+        print("pass")
         con.sendall(str.encode('1, 0'))
 
 
-    if opcheck == 3:
+    elif opcheck == 3:
         data_size = "14s"
         data = struct.unpack(data_size, message[int_size:])
         double_check = sys.getsizeof(data)
-
+        # print(double_check)
         br = random.randint(0, 100)  # noise
         if br < 50:
             double_check = 1
+            counter = 1
+            while double_check != 48:
+                con.send(bytes('1', "UTF-8"))
+                message = con.recv(1024)
+                data = struct.unpack(data_size, message[int_size:])
+                double_check = sys.getsizeof(data)
+                br = random.randint(0, 100)  # noise
+                if br < 50:
+                    double_check = 1
+                print(counter)
+                counter += 1
 
-        while double_check != 48:
-            con.send(bytes('1', "UTF-8"))
 
-            message = con.recv(1024)
-            data = struct.unpack(data_size, message[int_size:])
-            double_check = sys.getsizeof(data)
-            accident(10)
-
+        print("pass")
         message = bytes.decode(message)
         message = message.split()
         if 0 <= int(float(message[1])) <= 100 and 0 <= int(float(message[2])) <= 100:
             con.sendall(str.encode("\\3\\0"))
         else:
             con.sendall(str.encode("\\3\\1"))
-
-
-
-
-
-
-
-
-
-
-    # output1 = bytes.decode(output1)
-    # output1 = output1.split()
-    # opcheck = int(output1[0])
-
-    # if opcheck == 1:
-    #     sleep(0.5)
-    #     con.sendall(str.encode('1, 0'))
-    #
-    # if opcheck == 3:
-    #     if 0 <= int(output1[1]) <= 100 and 0 <= int(output1[2]) <= 100:
-    #         con.sendall(str.encode("\\3\\0"))
-    #     else:
-    #         con.sendall(str.encode("\\3\\1"))
-
-
